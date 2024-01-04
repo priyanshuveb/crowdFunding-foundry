3205de9df325314e1bf666c80e56a8bc70d659b4
priyanshuveb <priyanshubindal@gmail.com>
2024-01-04 07:22:52 +0530

    testing cheatcodes in foundry

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -9,17 +9,21 @@ import {FundMe} from "../src/FundMe.sol";
 import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
-
     FundMe fundMe;
-    DeployFundMe deployFundMe;
 
+    address immutable ALICE = makeAddr("alice");
+    uint256 constant INITIAL_BALANCE = 100e18;
+    uint256 constant SEND_VALUE = 1e18;
+    // setUp is an Forge Keyword: An optional function invoked before each test case is run.
     // deploying the contract
     function setUp() external {
         // us -> FundMeTest contract -> FundMe contract
         // owner of the FundMe contract is FundMeTest contract
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        deployFundMe = new DeployFundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+
+        vm.deal(ALICE, INITIAL_BALANCE);
     }
 
     function testMinimumDollarIsFive() public {
@@ -35,7 +39,37 @@ contract FundMeTest is Test {
 
     function testPriceFeedVersionIsAccurate() public {
         uint256 version = fundMe.getVersion();
-        assertEq(version,4);
+        assertEq(version, 4);
     }
 
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // The following should revert
+        fundMe.fund{value: 0}(); // send 0 ETH
+    }
+
+    function testFundUpdatesFundedDataStruct() public {
+        console.log(ALICE);
+        vm.deal(ALICE, INITIAL_BALANCE); // Funds the ALICE account with mentioned amount
+        vm.prank(ALICE); // The next transaction will be sent using this account
+        fundMe.fund{value: SEND_VALUE}();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(ALICE);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public {
+        fundMe.fund{value: SEND_VALUE}();
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, ALICE);
+    }
+
+    function testOnlyOwnerCanWithdraw() public {
+        vm.prank(ALICE);
+        fundMe.fund{value: SEND_VALUE}();
+
+        vm.expectRevert();
+        vm.prank(ALICE);
+        fundMe.withdraw();
+    }
 }
